8da7d57d6a989a2433b2f1ed877c4e14dc01b25b
https://github.com/mazharktk5 <mazharahmad862@gmail.com>
2024-11-18 09:17:24 -0800

    arrow function

---
+++ arrow fucntion/script.js
@@ -0,0 +1,20 @@
+// // arrow function
+
+// let sum = (a,b) =>{
+//     return a + b;
+// }
+// sum= sum(5,5);
+
+// console.log(sum); // Output: 10
+
+sum = (a,b) =>{
+    return a + b;
+}
+
+const multiply = (a,b) => {
+    return a * b;
+}
+
+const result = multiply(5,5);
+
+console.log(result); // Output: 25
