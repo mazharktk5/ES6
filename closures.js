e3c631bf549660c958369f18849f919c1dc0fa30
https://github.com/mazharktk5 <mazharahmad862@gmail.com>
2024-11-18 06:29:47 -0800

    closures

---
+++ closures.js
@@ -0,0 +1,17 @@
+// closures
+
+function outer() {
+    let outerVar = "I am a variable inside the outer function";
+    function inner() {
+        // let innerVar = "I am a variable inside the inner function";
+        console.log(outerVar); // I am a variable inside the outer function
+        // console.log(innerVar); // I am a variable inside the inner function
+    }
+    return inner;
+    }
+    innerfucntion = outer();
+    innerfucntion();
+
+    console.log(outerVar); // I am a variable inside
+
+    
